698fe4ee3194f2f4687e30e131d3170346ce9207
Coding Rooms <git@codingrooms.io>
2023-06-12 21:34:11 +0000

    commits 12 June

--- src/GenericsPractice/Pair.java
+++ src/GenericsPractice/Pair.java
@@ -1,9 +1,24 @@
 package GenericsPractice;
 // TODO: replace ... with appropriate code
 
-public class Pair<T ...> 
-{
+// new Pair<Integer>(2, 3).min() => 2
+// new Pair<String>("2", "3").min() => "2"
+
+
+/**
+ * A Pair represents a pair of elements of the same type that can be compared to each other.
+ * 
+ * @param <T> the type of the elements in the pair
+ */
+public class Pair<T extends Comparable<T>> 
+{ // 
+   /**
+    * The first element of the pair
+    */
    private T e1;
+   /** 
+    * The second element of the pair
+    */
    private T e2;
 
    /**
@@ -17,8 +32,17 @@ public class Pair<T ...>
       this.e2 = e2;
    }
 
-   public ... min()
+
+   /**
+    * Returns the minimum of the two elements
+    * @return whatever is feels like
+    */
+   public T min()
    {  
-      ...
+      if (e1.compareTo(e2) < 0) {
+         return e1;
+      } else {
+         return e2;
+      }
    }
 }
